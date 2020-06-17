9b77aa3e0c8f36c11d73ab65b7b7bce8e6a6645f
Eeveeee <fx8300gfe@gmail.com>
2020-06-17 22:28:01 +0300

    To the clients

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
-  $(".slider-small").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    speed: 800,
-    asNavFor: ".slider-big",
-  });
   $(".slider-big").slick({
     arrows: false,
     draggable: false,
     asNavFor: ".slider-small",
     fade: true,
+  });
+  $(".slider-small").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 800,
+    asNavFor: ".slider-big",
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
